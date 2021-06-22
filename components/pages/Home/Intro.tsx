bebaf69c4b7ccdcaf4370beacd877ac194921a20
Stanislav <verbatim8872@gmail.com>
2021-06-22 20:20:30 +0300

    add intro and some refactor

--- components/pages/Home/Intro.tsx
+++ components/pages/Home/Intro.tsx
@@ -3,8 +3,16 @@ import s from '../../../styles/components/pages/Home/Intro.module.scss'
 
 const Intro = () => {
     return (
-        <section>
-            
+        <section className={s.intro}>
+            <div className={s.info}>
+                <img src="/icons/logo.svg" alt="biocorrector" className={s.logo}/>
+                <h2 className={s.title}>Защита от внешнего энергетического воздействия</h2>
+                <h3 className={s.subtitle}>Подключек, гипноза, чужеродных лярв и потери энергии</h3>
+                <button className={s.button}>Выбрать биокорректор</button>
+            </div>
+            <div className={s.photo}>
+                <img src="/intro.png" alt="biocorrector" />
+            </div>
         </section>
     )
 }
