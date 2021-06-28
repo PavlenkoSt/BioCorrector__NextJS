9c8dc6be32b1f32cd0e5b210a9e71096e090b0df
Stanislav <verbatim8872@gmail.com>
2021-06-28 20:09:13 +0300

    full work

--- pages/api/reviews/index.ts
+++ pages/api/reviews/index.ts
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 
 const getReviews = (req: NextApiRequest, res: NextApiResponse) => {
-    const {data, pageCount} = mergeDataForPagination(reviews, +req.query.limit)
+    const {data, pageCount} = mergeDataForPagination(reviews)
 
     res.status(200).json({ data: data[+req.query.page - 1 || 0], pageCount })
 }
